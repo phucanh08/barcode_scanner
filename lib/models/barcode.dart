ddd7fd7e843075229c750891d5da8f3edf7f3683
Lê Phúc Anh <anhlp240198@gmail.com>
2025-04-10 23:30:29 +0700

    Update bcbp and bcwifi (#10)
    * feat: update bcbp and bc wifi
    
    * feat: update bcbp lib
    
    ---------
    
    Co-authored-by: AnhLP <anhlp@etc.vn>
    Co-authored-by: anhlp <phucanhdn08@gmail.com>

--- lib/models/barcode.dart
+++ lib/models/barcode.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:bcbp/bcbp.dart' show bcbpDecode;
 import '../gen/protos/protos.pb.dart' show BarcodeResult;
 
 class Barcode {
@@ -37,61 +37,24 @@ class Barcode {
 }
 
 Map<BarcodeType, BarcodeValue?> typeAndValueFromString(String rawValue) {
-  BarcodeType type = BarcodeType.unknown;
-  BarcodeValue? value;
   try {
-    // final jsonData = jsonDecode(rawValue);
-
-    // for (var e in BarcodeType.values) {
-    //   try {
-    //     switch (e) {
-    //       case BarcodeType.unknown:
-    //         break;
-    //       case BarcodeType.contactInfo:
-    //         value = BarcodeContactInfo.fromJson(jsonData);
-    //         break;
-    //       case BarcodeType.email:
-    //         value = BarcodeEmail.fromJson(jsonData);
-    //         break;
-    //       case BarcodeType.isbn:
-    //       // TODO: Handle this case.
-    //         break;
-    //       case BarcodeType.phone:
-    //         value = BarcodePhone.fromJson(jsonData);
-    //         break;
-    //       case BarcodeType.product:
-    //       // TODO: Handle this case.
-    //         break;
-    //       case BarcodeType.sms:
-    //       // TODO: Handle this case.
-    //         break;
-    //       case BarcodeType.text:
-    //       // TODO: Handle this case.
-    //         break;
-    //       case BarcodeType.url:
-    //         value = BarcodeUrl.fromJson(jsonData);
-    //         break;
-    //       case BarcodeType.wifi:
-    //         value = BarcodeWifi.fromJson(jsonData);
-    //         break;
-    //       case BarcodeType.geoCoordinates:
-    //       // TODO: Handle this case.
-    //         break;
-    //       case BarcodeType.calendarEvent:
-    //       // TODO: Handle this case.
-    //         break;
-    //       case BarcodeType.driverLicense:
-    //         value = BarcodeDriverLicense.fromJson(jsonData);
-    //         break;
-    //       case BarcodeType.boardingPass:
-    //       // TODO: Handle this case.
-    //         break;
-    //     }
-    //   } catch (_) {}
-    // }
+    for (var e in BarcodeType.values) {
+      switch (e) {
+        case BarcodeType.boardingPass:
+          final value = BarcodeBoardingPass.fromRawData(rawValue);
+          if (value != null) return {BarcodeType.boardingPass: value};
+          break;
+        case BarcodeType.wifi:
+          final value = BarcodeWifi.fromRawData(rawValue);
+          if (value != null) return {BarcodeType.wifi: value};
+          break;
+        default:
+          break;
+      }
+    }
   } catch (_) {}
 
-  return {type: value};
+  return {BarcodeType.unknown: null};
 }
 
 enum BarcodeFormat {
@@ -121,47 +84,81 @@ enum BarcodeType {
   /// Unknown Barcode value types.
   unknown,
 
-  /// Barcode value type for contact info.
-  contactInfo,
-
-  /// Barcode value type for email addresses.
-  email,
-
-  /// Barcode value type for ISBNs.
-  isbn,
-
-  /// Barcode value type for phone numbers.
-  phone,
-
-  /// Barcode value type for product codes.
-  product,
-
-  /// Barcode value type for SMS details.
-  sms,
-
-  /// Barcode value type for plain text.
-  text,
-
-  /// Barcode value type for URLs/bookmarks.
-  url,
-
   /// Barcode value type for Wi-Fi access point details.
   wifi,
-
-  /// Barcode value type for geo coordinates.
-  geoCoordinates,
-
-  /// Barcode value type for calendar events.
-  calendarEvent,
-
-  /// Barcode value type for driver's license data.
-  driverLicense,
 
   /// Barcode value type for boarding pass.
   boardingPass,
 }
 
 abstract class BarcodeValue {}
+
+class BarcodeBoardingPass extends BarcodeValue {
+  /// passengerName
+  final String? passengerName;
+
+  /// PNR
+  final String? operatingCarrierPNR;
+
+  /// IATA departure Airport
+  final String? departureAirport;
+
+  /// IATA arrival Airport
+  final String? arrivalAirport;
+
+  final String? flightNumber;
+
+  final DateTime? flightDate;
+
+  final String? seatNumber;
+
+  final DateTime? issuanceDate;
+
+  final String? compartmentCode;
+
+  final String? checkInSequenceNumber;
+
+  final String? airlineNumericCode;
+
+  BarcodeBoardingPass({
+    required this.passengerName,
+    required this.operatingCarrierPNR,
+    required this.departureAirport,
+    required this.arrivalAirport,
+    required this.flightNumber,
+    required this.flightDate,
+    required this.seatNumber,
+    required this.issuanceDate,
+    required this.compartmentCode,
+    required this.checkInSequenceNumber,
+    required this.airlineNumericCode,
+  });
+
+  static BarcodeBoardingPass? fromRawData(String? data) {
+    try {
+      final result = bcbpDecode(data ?? "");
+      final legs = result.data?.legs ?? [];
+      if (legs.isEmpty) {
+        return null;
+      }
+      return BarcodeBoardingPass(
+        passengerName: result.data?.passengerName,
+        operatingCarrierPNR: legs.first.operatingCarrierPNR,
+        departureAirport: legs.first.departureAirport,
+        arrivalAirport: legs.first.arrivalAirport,
+        flightNumber: legs.first.flightNumber,
+        flightDate: legs.first.flightDate,
+        seatNumber: legs.first.seatNumber,
+        issuanceDate: result.data?.issuanceDate,
+        compartmentCode: legs.first.compartmentCode,
+        checkInSequenceNumber: legs.first.checkInSequenceNumber,
+        airlineNumericCode: legs.first.airlineNumericCode,
+      );
+    } catch (e) {
+      return null;
+    }
+  }
+}
 
 /// Stores wifi info obtained from a barcode.
 class BarcodeWifi extends BarcodeValue {
@@ -172,416 +169,51 @@ class BarcodeWifi extends BarcodeValue {
   final String? password;
 
   /// Encryption type of wifi.
-  final int? encryptionType;
+  final String? encryptionType;
+
+  final bool? hidden;
 
   /// Constructor to create an instance of [BarcodeWifi].
-  BarcodeWifi({this.ssid, this.password, this.encryptionType});
+  BarcodeWifi({this.ssid, this.password, this.encryptionType, this.hidden});
 
   /// Returns an instance of [BarcodeWifi] from a given [json].
   factory BarcodeWifi.fromJson(Map<dynamic, dynamic> json) => BarcodeWifi(
         ssid: json['ssid'],
         password: json['password'],
-        encryptionType: json['encryption'],
+        encryptionType: json['type'],
+        hidden: json['hidden'] == true,
       );
-}
 
-/// Stores url info of the bookmark obtained from a barcode.
-class BarcodeUrl extends BarcodeValue {
-  /// String having the url address of bookmark.
-  final String? url;
-
-  /// Title of the bookmark.
-  final String? title;
-
-  /// Constructor to create an instance of [BarcodeUrl].
-  BarcodeUrl({this.url, this.title});
-
-  /// Returns an instance of [BarcodeUrl] from a given [json].
-  factory BarcodeUrl.fromJson(Map<dynamic, dynamic> json) => BarcodeUrl(
-        url: json['url'],
-        title: json['title'],
-      );
-}
-
-/// The type of email for [BarcodeEmail.type].
-enum BarcodeEmailType {
-  /// Unknown email type.
-  unknown,
-
-  /// Barcode work email type.
-  work,
-
-  /// Barcode home email type.
-  home,
-}
-
-/// Stores an email message obtained from a barcode.
-class BarcodeEmail extends BarcodeValue {
-  /// Type of the email sent.
-  final BarcodeEmailType? type;
-
-  /// Email address of sender.
-  final String? address;
-
-  /// Body of the email.
-  final String? body;
-
-  /// Subject of email.
-  final String? subject;
-
-  /// Constructor to create an instance of [BarcodeEmail].
-  BarcodeEmail({this.type, this.address, this.body, this.subject});
-
-  /// Returns an instance of [BarcodeEmail] from a given [json].
-  factory BarcodeEmail.fromJson(Map<dynamic, dynamic> json) => BarcodeEmail(
-        type: BarcodeEmailType.values[json['emailType']],
-        address: json['address'],
-        body: json['body'],
-        subject: json['subject'],
-      );
-}
-
-/// The type of phone number for [BarcodePhone.type].
-enum BarcodePhoneType {
-  /// Unknown phone type.
-  unknown,
-
-  /// Barcode work phone type.
-  work,
-
-  /// Barcode home phone type.
-  home,
-
-  /// Barcode fax phone type.
-  fax,
-
-  /// Barcode mobile phone type.
-  mobile,
-}
-
-/// Stores a phone number obtained from a barcode.
-class BarcodePhone extends BarcodeValue {
-  /// Type of the phone number.
-  final BarcodePhoneType? type;
-
-  /// Phone number.
-  final String? number;
-
-  /// Constructor to create an instance of [BarcodePhone].
-  BarcodePhone({this.type, this.number});
-
-  /// Returns an instance of [BarcodePhone] from a given [json].
-  factory BarcodePhone.fromJson(Map<dynamic, dynamic> json) => BarcodePhone(
-        type: BarcodePhoneType.values[json['phoneType']],
-        number: json['number'],
-      );
-}
-
-/// Stores an SMS message obtained from a barcode.
-class BarcodeSMS extends BarcodeValue {
-  /// Message present in the SMS.
-  final String? message;
-
-  /// Phone number of the sender.
-  final String? phoneNumber;
-
-  /// Constructor to create an instance of [BarcodeSMS].
-  BarcodeSMS({this.message, this.phoneNumber});
-
-  /// Returns an instance of [BarcodeSMS] from a given [json].
-  factory BarcodeSMS.fromJson(Map<dynamic, dynamic> json) => BarcodeSMS(
-        message: json['message'],
-        phoneNumber: json['number'],
-      );
-}
-
-/// Stores GPS coordinates obtained from a barcode.
-class BarcodeGeoPoint extends BarcodeValue {
-  /// Latitude co-ordinates of the location.
-  final double? latitude;
-
-  //// Longitude co-ordinates of the location.
-  final double? longitude;
-
-  /// Constructor to create an instance of [BarcodeGeoPoint].
-  BarcodeGeoPoint({this.latitude, this.longitude});
-
-  /// Returns an instance of [BarcodeGeoPoint] from a given [json].
-  factory BarcodeGeoPoint.fromJson(Map<dynamic, dynamic> json) =>
-      BarcodeGeoPoint(
-        latitude: json['latitude'],
-        longitude: json['longitude'],
-      );
-}
-
-/// Stores driver’s license or ID card data representation obtained from a barcode.
-class BarcodeDriverLicense extends BarcodeValue {
-  /// City of holder's address.
-  final String? addressCity;
-
-  /// State of the holder's address.
-  final String? addressState;
-
-  /// Zip code code of the holder's address.
-  final String? addressZip;
-
-  /// Street of the holder's address.
-  final String? addressStreet;
-
-  /// Date on which the license was issued.
-  final String? issueDate;
-
-  /// Birth date of the card holder.
-  final String? birthDate;
-
-  /// Expiry date of the license.
-  final String? expiryDate;
-
-  /// Gender of the holder.
-  final String? gender;
-
-  /// Driver license ID.
-  final String? licenseNumber;
-
-  /// First name of the holder.
-  final String? firstName;
-
-  /// Last name of the holder.
-  final String? lastName;
-
-  /// Country of the holder.
-  final String? country;
-
-  /// Constructor to create an instance of [BarcodeDriverLicense].
-  BarcodeDriverLicense({
-    this.addressCity,
-    this.addressState,
-    this.addressZip,
-    this.addressStreet,
-    this.issueDate,
-    this.birthDate,
-    this.expiryDate,
-    this.gender,
-    this.licenseNumber,
-    this.firstName,
-    this.lastName,
-    this.country,
-  });
-
-  /// Returns an instance of [BarcodeDriverLicense] from a given [json].
-  factory BarcodeDriverLicense.fromJson(Map<dynamic, dynamic> json) =>
-      BarcodeDriverLicense(
-        addressCity: json['addressCity'],
-        addressState: json['addressState'],
-        addressZip: json['addressZip'],
-        addressStreet: json['addressStreet'],
-        issueDate: json['issueDate'],
-        birthDate: json['birthDate'],
-        expiryDate: json['expiryDate'],
-        gender: json['gender'],
-        licenseNumber: json['licenseNumber'],
-        firstName: json['firstName'],
-        lastName: json['lastName'],
-        country: json['country'],
-      );
-}
-
-/// Stores a person’s or organization’s business card obtained from a barcode.
-class BarcodeContactInfo extends BarcodeValue {
-  /// Contact person's addresses.
-  final List<BarcodeAddress> addresses;
-
-  /// Email addresses of the contact person.
-  final List<BarcodeEmail> emails;
-
-  /// Phone numbers of the contact person.
-  final List<BarcodePhone> phoneNumbers;
-
-  /// First name of the contact person.
-  final String? firstName;
-
-  /// Middle name of the person.
-  final String? middleName;
-
-  /// Last name of the person.
-  final String? lastName;
-
-  /// Properly formatted name of the person.
-  final String? formattedName;
-
-  /// Name prefix.
-  final String? prefix;
-
-  /// Name pronunciation.
-  final String? pronunciation;
-
-  /// Job title.
-  final String? jobTitle;
-
-  /// Organization of the contact person.
-  final String? organizationName;
-
-  /// Url's of contact person.
-  final List<String> urls;
-
-  /// Constructor to create an instance of [BarcodeContactInfo].
-  BarcodeContactInfo({
-    required this.addresses,
-    required this.emails,
-    required this.phoneNumbers,
-    required this.urls,
-    this.firstName,
-    this.middleName,
-    this.lastName,
-    this.formattedName,
-    this.prefix,
-    this.pronunciation,
-    this.jobTitle,
-    this.organizationName,
-  });
-
-  /// Returns an instance of [BarcodeContactInfo] from a given [json].
-  factory BarcodeContactInfo.fromJson(Map<dynamic, dynamic> json) =>
-      BarcodeContactInfo(
-        addresses: _getBarcodeAddresses(json),
-        emails: _getBarcodeEmails(json),
-        phoneNumbers: _getBarcodePhones(json),
-        firstName: json['firstName'],
-        middleName: json['middleName'],
-        lastName: json['lastName'],
-        formattedName: json['formattedName'],
-        prefix: json['prefix'],
-        pronunciation: json['pronunciation'],
-        jobTitle: json['jobTitle'],
-        organizationName: json['organization'],
-        urls: _getUrls(json['urls']),
-      );
-}
-
-/// Stores a calendar event obtained from a barcode.
-class BarcodeCalenderEvent extends BarcodeValue {
-  /// Description of the event.
-  final String? description;
-
-  /// Location of the event.
-  final String? location;
-
-  /// Status of the event -> whether the event is completed or not.
-  final String? status;
-
-  /// A short summary of the event.
-  final String? summary;
-
-  /// A person or the organisation who is organising the event.
-  final String? organizer;
-
-  /// Start DateTime of the calender event.
-  final DateTime? start;
-
-  /// End DateTime of the calender event.
-  final DateTime? end;
-
-  /// Constructor to create an instance of [BarcodeCalenderEvent].
-  BarcodeCalenderEvent({
-    this.description,
-    this.location,
-    this.status,
-    this.summary,
-    this.organizer,
-    this.start,
-    this.end,
-  });
-
-  /// Returns an instance of [BarcodeCalenderEvent] from a given [json].
-  factory BarcodeCalenderEvent.fromJson(Map<dynamic, dynamic> json) =>
-      BarcodeCalenderEvent(
-        description: json['description'],
-        location: json['location'],
-        status: json['status'],
-        summary: json['summary'],
-        organizer: json['organizer'],
-        start: _getDateTime(json['start']),
-        end: _getDateTime(json['end']),
-      );
-}
-
-/// Address type constants for [BarcodeAddress.type]
-enum BarcodeAddressType {
-  /// Barcode unknown address type.
-  unknown,
-
-  /// Barcode work address type.
-  work,
-
-  /// Barcode home address type.
-  home,
-}
-
-/// Stores an address obtained from a barcode.
-class BarcodeAddress {
-  /// Address lines found.
-  final List<String> addressLines;
-
-  /// The address type.
-  final BarcodeAddressType? type;
-
-  /// Constructor to create an instance of [BarcodeAddress].
-  BarcodeAddress({required this.addressLines, this.type});
-
-  /// Returns an instance of [BarcodeAddress] from a given [json].
-  factory BarcodeAddress.fromJson(Map<dynamic, dynamic> json) {
-    final lines = <String>[];
-    for (final dynamic line in json['addressLines']) {
-      lines.add(line);
+  static BarcodeWifi? fromRawData(String? data) {
+    try {
+      final wifiData = data ?? "";
+
+      if (!wifiData.startsWith('WIFI:')) {
+        return null;
+      }
+
+      final regex = RegExp(r'([TSPH]):([^;]*)');
+      final matches = regex.allMatches(wifiData);
+
+      final Map<String, String> keyMap = {
+        'T': 'type',
+        'S': 'ssid',
+        'P': 'password',
+        'H': 'hidden',
+      };
+
+      final resultJson = <String, String>{};
+      for (final match in matches) {
+        final key = match.group(1);
+        final value = match.group(2);
+        if (key != null && value != null) {
+          resultJson[keyMap[key] ?? key] = value;
+        }
+      }
+
+      return BarcodeWifi.fromJson(resultJson);
+    } catch (_) {
+      return null;
     }
-    return BarcodeAddress(
-      addressLines: lines,
-      type: BarcodeAddressType.values[json['addressType']],
-    );
   }
-}
-
-DateTime? _getDateTime(dynamic barcodeData) {
-  if (barcodeData is double) {
-    return DateTime.fromMillisecondsSinceEpoch(barcodeData.toInt() * 1000);
-  } else if (barcodeData is String) {
-    return DateTime.parse(barcodeData);
-  }
-  return null;
-}
-
-List<BarcodeAddress> _getBarcodeAddresses(dynamic json) {
-  final list = <BarcodeAddress>[];
-  json['addresses']?.forEach((address) {
-    list.add(BarcodeAddress.fromJson(address));
-  });
-  return list;
-}
-
-List<BarcodeEmail> _getBarcodeEmails(dynamic json) {
-  final list = <BarcodeEmail>[];
-  json['emails']?.forEach((email) {
-    email['type'] = BarcodeType.email.index;
-    email['format'] = json['format'];
-    list.add(BarcodeEmail.fromJson(email));
-  });
-  return list;
-}
-
-List<BarcodePhone> _getBarcodePhones(dynamic json) {
-  final list = <BarcodePhone>[];
-  json['phones']?.forEach((phone) {
-    phone['type'] = BarcodeType.phone.index;
-    phone['format'] = json['format'];
-    list.add(BarcodePhone.fromJson(phone));
-  });
-  return list;
-}
-
-List<String> _getUrls(dynamic json) {
-  final list = <String>[];
-  json.forEach((url) {
-    list.add(url.toString());
-  });
-  return list;
 }
